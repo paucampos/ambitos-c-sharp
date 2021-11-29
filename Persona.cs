8f3ba63a98681e8098bed25b819ffacb7bbccb72
Pau Campos <pau.campos89@gmail.com>
2021-11-28 22:00:11 -0300

    Clase persona

---
+++ Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambitos
+{
+    class Persona
+    {
+        public string rut;
+        public int edad;
+        public string nombre;
+        public string cargo;
+        private int sueldo;
+
+        // Método
+        private int calculoSueldo(string cargoPersona)
+        {
+            if (cargoPersona == "Analista")
+            {
+                return 1700000;
+            }
+            else if(cargo == "Operador")
+            {
+                return 800000;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
